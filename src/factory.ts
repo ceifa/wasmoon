8e818b6ddee561a024d0966f25fe13e1f0100872
ceifa <gabrinelson27@gmail.com>
2021-02-02 20:28:56 -0300

    Update lua and add a default external wasm url when running on browser

--- src/factory.ts
+++ src/factory.ts
@@ -4,13 +4,20 @@ import LuaWasm from "./luawasm"
 export default class LuaFactory {
     private module: LuaWasm
 
-    constructor(private customWasmName: string) {
+    constructor(private customWasmUri: string = undefined) {
+        if (this.customWasmUri === undefined) {
+            const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
+
+            if (isBrowser) {
+                this.customWasmUri = 'http://unpkg.com/wasmoon/dist/glue.wasm';
+            }
+        }
     }
 
     public async createEngine(openStandardLibs: boolean = true): Promise<Lua> {
         if (!this.module) {
             this.module = new LuaWasm()
-            await this.module.initialize(this.customWasmName)
+            await this.module.initialize(this.customWasmUri)
         }
 
         return new Lua(this.module, openStandardLibs)
