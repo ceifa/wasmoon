c601ee4b47e260b8f257809f74960ecf23c8a810
ceifa <gabrinelson27@gmail.com>
2022-12-11 08:51:28 -0300

    Convert prettier to json

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    singleQuote: true,
-    trailingComma: 'all',
-    endOfLine: 'lf',
-    quoteProps: 'consistent',
-    semi: false,
-    printWidth: 140,
-    tabWidth: 4,
-}
