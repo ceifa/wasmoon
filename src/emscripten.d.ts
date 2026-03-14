238de2889670da7eecbec30b8d1090fc67aa5677
Gabriel Francisco <gabrinelson27@gmail.com>
2026-03-13 23:11:56 -0300

    Minify the local emscripten ambient declarations further by shortening parameter names and whitespace\n\nResult: {"status":"keep","tarball_kb":156.052}

--- src/emscripten.d.ts
+++ src/emscripten.d.ts
@@ -1,12 +1,12 @@
 declare namespace Emscripten{type JSType='number'|'string'|'array'|'boolean';type TypeCompatibleWithC=number|bigint|string|boolean|null|undefined|ArrayLike<number>;interface FileSystemType{}}
-declare interface EmscriptenModule{_free(pointer:number):void}
-declare function ccall(name:string,returnType:Emscripten.JSType|null,argTypes:Emscripten.JSType[],args:Emscripten.TypeCompatibleWithC[]):any
-declare function addFunction(fn:(...args:number[])=>number|void,signature?:string):number
-declare function removeFunction(funcPtr:number):void
-declare function setValue(ptr:number,value:number,type:string):void
-declare function getValue(ptr:number,type:string):number
-declare function allocateUTF8(value:string):number
-declare function lengthBytesUTF8(value:string):number
-declare function stringToUTF8(value:string,outPtr:number,maxBytesToWrite:number):void
-declare function intArrayFromString(value:string,dontAddNull?:boolean):number[]
-declare function UTF8ToString(ptr:number):string
+declare interface EmscriptenModule{_free(n:number):void}
+declare function ccall(n:string,r:Emscripten.JSType|null,t:Emscripten.JSType[],a:Emscripten.TypeCompatibleWithC[]):any
+declare function addFunction(f:(...a:number[])=>number|void,s?:string):number
+declare function removeFunction(n:number):void
+declare function setValue(p:number,v:number,t:string):void
+declare function getValue(p:number,t:string):number
+declare function allocateUTF8(s:string):number
+declare function lengthBytesUTF8(s:string):number
+declare function stringToUTF8(s:string,p:number,m:number):void
+declare function intArrayFromString(s:string,n?:boolean):number[]
+declare function UTF8ToString(p:number):string
